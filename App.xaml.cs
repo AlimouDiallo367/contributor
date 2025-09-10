fe1b7d364944cfd2d8db47251f1e5cf83c0f54d4
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-09-10 12:14:01 -0400

    Starting projects, mvvm structure done

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TP1_Donateurs
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Models/Contribution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_Donateurs.Models
+{
+    public class Contribution
+    {
+        private string _type;
+        private string _nom;
+        private string _prenom;
+        private decimal _montant;
+        private int _nbVersements;
+        private string _municipalite;
+        private string _codePostal;
+        private string _parti;
+        private string _candidat;
+        private DateTime _dateEvenement;
+        private int anneeFinanciere;
+    }
+}
