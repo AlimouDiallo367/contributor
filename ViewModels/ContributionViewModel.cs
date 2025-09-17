2bf9d1bd1c39e7a47eae4c353fe907c398e0139e
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-09-17 11:22:56 -0400

    ImportCSV file with OpenDialog done

--- ViewModels/ContributionViewModel.cs
+++ ViewModels/ContributionViewModel.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TP1_Donateurs.Models;
+using TP1_Donateurs.ViewModels.Commands;
 
 namespace TP1_Donateurs.ViewModels
 {
     public class ContributionViewModel : BaseViewModel
     {
+        #region RelayCommands
+        public RelayCommand CmdImporterFichier {  get; private set; }
+        #endregion
+
+
+        private AnalyseurContributions analyseur;
         private string _type, _nom, _prenom, _municipalite, _codePostal, _parti, _candidat;
         private decimal _montant;
         private int _nbVersements, anneeFinanciere;
@@ -20,9 +28,26 @@ namespace TP1_Donateurs.ViewModels
         public ContributionViewModel()
         {
             LesContributions = new ObservableCollection<Contribution>();
-
-            string csv = TP1_Donateurs.Properties.OthersResources.contributions;
+            CmdImporterFichier = new RelayCommand(ImporterFichier, null);
+           
         }
+
+        #region Boutons
+        private void ImporterFichier(object? obj)
+        {
+            var dialog = new OpenFileDialog(); 
+
+            if (dialog.ShowDialog() == true)
+            {
+                analyseur = new AnalyseurContributions(dialog.FileName);
+                foreach (var contribution in analyseur.Contributions)
+                {
+                    LesContributions.Add(contribution); 
+                }
+            }
+
+        }
+        #endregion
 
     }
 }
