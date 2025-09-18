604adbd2aa4192b42f664c19c816fb50e42d5852
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-09-18 09:46:03 -0400

    ConfigurationView UI and Switching from MainView

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -3,23 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TP1_Donateurs.ViewModels.Commands;
 
 namespace TP1_Donateurs.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        private ContributionViewModel _contributionViewModel;
+        #region RelayCommands
+        public RelayCommand CmdGotoConfiguration {  get; private set; }
+        #endregion
+
         private BaseViewModel _currentViewModel;
-        public MainViewModel()
-        {
-            _contributionViewModel = new ContributionViewModel();
-            _currentViewModel = _contributionViewModel;
-        }
+        private ContributionViewModel _contributionViewModel;
+        private ConfigurationViewModel _configurationViewModel;
 
         public BaseViewModel CurrentViewModel
         {
             get { return _currentViewModel; }
-            set { _currentViewModel = value; }
+            set
+            { 
+                _currentViewModel = value;
+            }
+        }
+
+        public MainViewModel()
+        {
+            _contributionViewModel = new ContributionViewModel();
+            _configurationViewModel = new ConfigurationViewModel();
+            _currentViewModel = _contributionViewModel;
+
+            CmdGotoConfiguration = new RelayCommand(GotoConfiguration, null);
+        }
+        
+        private void GotoConfiguration(object? obj)
+        {
+            //CurrentViewModel = _configurationViewModel;
+            var configurationWindow = new Views.ConfigurationView();
+            configurationWindow.DataContext = _configurationViewModel;
+            configurationWindow.Show();
         }
     }
 }
--- Views/ConfigurationView.xaml.cs
+++ Views/ConfigurationView.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TP1_Donateurs.Views
@@ -18,7 +17,7 @@ namespace TP1_Donateurs.Views
     /// <summary>
     /// Logique d'interaction pour ConfigurationView.xaml
     /// </summary>
-    public partial class ConfigurationView : UserControl
+    public partial class ConfigurationView : Window
     {
         public ConfigurationView()
         {
